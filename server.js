b5cf0c47582ef7a1bac193b7f74c136f1c0ee4aa
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-07 22:41:20 -0500

    finished server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.static('public'));
 // Replace the placeholder with your Atlas connection string
 const dbConnectionStr = process.env.DB_STRING;
 const dbName = 'rappersDb';
-let db;
+let collection;
 
 //Note: Worked with Mike Pennisi
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -27,7 +27,7 @@ const client = new MongoClient(dbConnectionStr, {
 async function connectToDB() {
   // Connect the client to the server (optional starting in v4.7)
   await client.connect();
-  db = client.db(dbName);
+  collection = client.db(dbName).collection('rappers');
   console.log('You successfully connected to MongoDB!');
 }
 
@@ -48,31 +48,48 @@ connectToDB()
   )
   .catch(console.dir);
 
-//READ (get all rappers)
+//READ (get all rappers, return a rappers array)
 app.get('/', async (req, res) => {
   try {
-    const coll = db.collection('rappers');
-    const allRappers = await coll.find().toArray();
-    
+    const rappers = await collection.find().toArray();
+    res.status(200).render('index.ejs', { rappers });
   } catch (error) {
-    console.log(error);
+    console.error(error);
+    res.status(500).send('Error rendering page');
   }
 });
 //CREATE (add a rapper to db)
-app.post('/rappers', async (req, res) => {
+app.post('/addRapper', async (req, res) => {
   try {
-    const { name, birthDate } = req.body;
-    const coll = db.collection('rappers');
-    const dbResponse = await coll.insertOne({ name, birthDate });
-    console.log(dbResponse);
-    res.end();
+    await collection.insertOne({ ...req.body, likes: 0 });
+    res.redirect('/');
   } catch (error) {
     console.log(error);
   }
 });
 
 //UPDATE (edit a rapper's likes)
-app.post('/rappers:id')
+app.put('/addLike', async (req, res) => {
+  try {
+    const { name, birthName } = req.body;
+    const coll = db.collection('rappers');
+    const rapper = await coll.findOne({
+      name, birthName
+    });
+    if (!rapper) {
+      res.status(404).send('Rapper not found');
+    }
+    await coll.updateOne(
+      { name, birthName },
+      { $set: { likes: rapper.likes + 1 } }
+    );
+    res.status(200).send('Rapper likes updated');
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error updating likes');
+  }
+}
+);
 
 //DELETE (delete a rapper from db)
-app.delete('/rappers:id')
+// app.delete('/rappers:id');
