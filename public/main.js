4b7db8b27a8c6314d724ff74cf46956dea120c99
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-11 15:45:19 -0500

    delete console logs

--- public/main.js
+++ public/main.js
@@ -1,13 +1,13 @@
 const thumbUps = Array.from(document.getElementsByClassName('fa-thumbs-up'));
+const thumbsDown = Array.from(document.getElementsByClassName('fa-thumbs-down'));
 const trashCans = Array.from(document.querySelectorAll('.fa-trash'));
+
 
 thumbUps.forEach(function (element, i) {
   element.addEventListener('click', function () {
-    // console.log(`clicked ${i}`);
-    console.log(this.parentNode.parentNode.parentNode.parentNode.childNodes);
+
     const name = this.parentNode.parentNode.parentNode.parentNode.childNodes[1].innerText.trim();
     const birthDate = this.parentNode.parentNode.parentNode.parentNode.childNodes[3].innerText.trim();
-    console.log({ name, birthDate})
    
     fetch('addLike', {
       method: 'put',
@@ -34,7 +34,6 @@ trashCans.forEach(function (element, i) {
       ancestor.childNodes[1].innerText.trim();
     const birthDate =
       ancestor.childNodes[3].innerText.trim();
-    console.log({ name, birthDate });
 
     fetch('deleteRapper', {
       method: 'delete',
@@ -58,3 +57,33 @@ trashCans.forEach(function (element, i) {
 });
 
 
+thumbsDown.forEach(function (element, i) {
+  element.addEventListener('click', function () {
+    // console.log(`clicked ${i}`);
+    console.log(this.parentNode.parentNode.parentNode.parentNode.childNodes);
+    const name =
+      this.parentNode.parentNode.parentNode.parentNode.childNodes[1].innerText.trim();
+    const birthDate =
+      this.parentNode.parentNode.parentNode.parentNode.childNodes[3].innerText.trim();
+    console.log({ name, birthDate });
+
+    fetch('subtractLike', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name,
+        birthDate,
+      }),
+    })
+      .then((response) => {
+        if (response.ok) return response.text();
+        else
+          console.error(
+            `error: status: ${response.status}, statusText: ${response.statusText}`
+          );
+      })
+      .then((data) => {
+        window.location.reload(true);
+      });
+  });
+});
