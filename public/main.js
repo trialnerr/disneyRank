19508bac585b548607ea7a7216345bd2bd448153
trialnerr <54381853+trialnerr@users.noreply.github.com>
2024-11-16 21:43:03 -0500

    Remove console log from main.js

--- public/main.js
+++ public/main.js
@@ -8,7 +8,7 @@ thumbUps.forEach(function (element, i) {
 
     const name = this.parentNode.parentNode.parentNode.parentNode.childNodes[1].innerText.trim();
     const song = this.parentNode.parentNode.parentNode.parentNode.childNodes[3].innerText.trim();
-    console.log({ name, song })
+  
     fetch('likeMovie', {
       method: 'put',
       headers: { 'Content-Type': 'application/json' },
